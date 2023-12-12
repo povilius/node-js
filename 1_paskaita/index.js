43135ed677edfa163f67d6ed90ccda1949e54d2d
povilius <povilius@yahoo.com>
2023-12-12 20:58:32 +0200

    Added first node.js

---
+++ 1_paskaita/index.js
@@ -0,0 +1,7 @@
+const casual = require('casual')
+
+// console.log(casual.city)
+
+console.log(`${casual.city} ${casual.integer(from = 0, to = 10)}`)
+
+console.log(`${casual.name_prefix} ${casual.first_name} ${casual.last_name}`)
